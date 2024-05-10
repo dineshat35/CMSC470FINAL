05d2e0854ad925077401bd29af085bd7a3deecf3
dineshat35 <dineshat35@gmail.com>
2024-05-10 15:42:06 -0700

    final draft on my end of both the quizbowl wrapper and pipeline

--- src/quizbowl_model.py
+++ src/quizbowl_model.py
@@ -37,80 +37,62 @@ kTOY_DATA = {"tiny": [{"text": "capital England", "page": "London"},
                       {'text': "located outside Boston, the oldest University in the United States",
                            "top": 'College_of_William_&_Mary', "second": 'Rhode_Island'}]
                 }
+
 class QuizBowlModel:
     def __init__(self):
-        self.load_models()
+        model_configs = {
+            'flan-t5-large': {'model': 'google/flan-t5-large', 'tokenizer': 'google/flan-t5-large'},
+            'flan-t5-small': {'model': 'google/flan-t5-small', 'tokenizer': 'google/flan-t5-small'},
+            'flan-t5-base': {'model': 'google/flan-t5-base', 'tokenizer': 'google/flan-t5-base'},
+            'flan-t5-finetuned': {'model': 'dbalasub/finetuned-t5-qanta', 'tokenizer': 'dbalasub/finetuned-t5-qanta'}
 
-    def load_models(self):
-        """Load all models"""
-        model_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'models', 't5-model-params')
-        self.load_seq2seq_model(model_dir)
-        self.load_flan_models('google/flan-t5-large', 'google/flan-t5-small')
+        }
+        self.models = {}
+        self.tokenizers = {}
+        self.load_models(model_configs)
 
-    def load_seq2seq_model(self, model_dir):
-        """Load saved models"""
-        self.test_tokenizer = AutoTokenizer.from_pretrained(model_dir)
-        self.test_model = AutoModelForSeq2SeqLM.from_pretrained(model_dir)
-        self.test_model.eval()
-
-    def load_flan_models(self, large_model_id, small_model_id):
-        """Load hugging face models."""        
-        self.tokenizer_flan_t5 = AutoTokenizer.from_pretrained(large_model_id)
-        self.model_flan_t5 = AutoModelForSeq2SeqLM.from_pretrained(large_model_id)
-        self.tokenizer_t5 = AutoTokenizer.from_pretrained(small_model_id)
-        self.model_t5 = AutoModelForSeq2SeqLM.from_pretrained(small_model_id)
+    def load_models(self, model_configs):
+        """Load multiple models based on configuration."""
+        for model_name, config in model_configs.items():
+            tokenizer = AutoTokenizer.from_pretrained(config['tokenizer'])
+            model = AutoModelForSeq2SeqLM.from_pretrained(config['model'])
+            model.eval()
+            self.models[model_name] = model
+            self.tokenizers[model_name] = tokenizer
 
     def guess_and_buzz(self, question_texts):
-        """Generate answers from all models for given questions"""
-        total_answers = self.generate_answers(question_texts)
-        # Display the model's guesses before voting
-        # print("Answers Before Voting Mechanism:")
+        total_answers = [self.generate_answers(question) for question in question_texts]
+        for question, model_answers in zip(question_texts, total_answers):
+            print(f"{question}\nModel Guesses: {model_answers}\n")
+        return [self.ensemble_tfidf_voting(answers) for answers in total_answers]
 
-        # for question, model_answers in zip(question_texts, total_answers):
-        #     print(f"{question}\nModel Guesses: {model_answers}\n\n")
-        return self.ensemble_tfidf_voting(total_answers)
+    def generate_answers(self, question):
+        raw_answers = []
+        for model_name, model in self.models.items():
+            tokenizer = self.tokenizers[model_name]
+            input_ids = tokenizer(question, return_tensors="pt", padding=True, truncation=True).input_ids
+            with torch.no_grad():
+                outputs = model.generate(input_ids, max_new_tokens=5, output_scores=True, return_dict_in_generate=True)
+            decoded_text = tokenizer.decode(outputs.sequences[0], skip_special_tokens=True)
+            confidence_score = self.calculate_confidence(outputs.scores)
+            raw_answers.append((decoded_text, confidence_score))
 
-    def generate_answers(self, question_texts):
-        """Generate answers from each model."""
-        # Tokenize and generate answers using each model
-        return [(self.decode_answer(self.model_flan_t5, self.tokenizer_flan_t5, question),
-                 self.decode_answer(self.model_t5, self.tokenizer_t5, question),
-                 self.decode_answer(self.test_model,self.test_tokenizer , question))
-                for question in question_texts]
-
-    def decode_answer(self, model, tokenizer, input_text):
-        input_ids = tokenizer(input_text, return_tensors="pt", padding=True, truncation=True)
-        with torch.no_grad():
-            outputs = model.generate(**input_ids, max_new_tokens=5, output_scores=True, return_dict_in_generate=True)
+        total_scores = sum([score for _, score in raw_answers])
+        answers = [(text, score / total_scores if total_scores > 0 else 0) for text, score in raw_answers]
         
-        decoded_text = tokenizer.decode(outputs.sequences[0], skip_special_tokens=True)
-        
-        if outputs.scores:
-            log_probs = [torch.nn.functional.log_softmax(score, dim=-1) for score in outputs.scores]
-            scores = []
-            for i in range(len(outputs.sequences[0]) - 1):
-                selected_log_prob = log_probs[i][0, outputs.sequences[0][i + 1]].item()
-                scores.append(selected_log_prob)
-            confidence_score = np.exp(np.mean(scores))
+        return answers
+
+    def calculate_confidence(self, scores):
+        if scores:
+            log_probs = [torch.nn.functional.log_softmax(score, dim=-1) for score in scores]
+            selected_log_probs = [log_probs[i][0, scores[i].argmax()].item() for i in range(len(log_probs))]
+            confidence_score = np.exp(np.mean(selected_log_probs))
         else:
             confidence_score = None
+        return confidence_score
 
-        return decoded_text, confidence_score
-
-    def ensemble_tfidf_voting(self, all_answers):
-        """Find answer with highest confidence"""
-        for answers in all_answers:
-            highest_confidence_answer = max(answers, key=lambda x: x[1])
-            yield {'guess': highest_confidence_answer[0], 'confidence': highest_confidence_answer[1]}
-
-        # for answers in all_answers:
-        #     texts = [answer[0] for answer in answers]
-            
-        #     vectorizer = TfidfVectorizer()
-        #     tfidf_matrix = vectorizer.fit_transform(texts)
-        #     cosine_scores = cosine_similarity(tfidf_matrix)
-        #     most_similar_index = np.argmax(np.mean(cosine_scores, axis=0))
-        #     yield {'guess': answers[most_similar_index][0], 'confidence': answers[most_similar_index][1]}
+    def ensemble_tfidf_voting(self, answers):
+        return max(answers, key=lambda x: x[1]) if answers else (None, 0)
 
 if __name__ == "__main__":
     # Initialize the QuizBowlModel
--- src/test_ensemble_pipeline.py
+++ src/test_ensemble_pipeline.py
@@ -23,102 +23,105 @@ class TestEnsembleQAPipeline(Text2TextGenerationPipeline):
     def postprocess(self, model_outputs):
         results = {}
         for output in model_outputs:
-            guess_text = output['guess']
-            confidence = output['confidence']
+            guess_text = output[0]
+            confidence = output[1]
             results = {'guess': guess_text, 'confidence': confidence}
         return results
 
-# From class eval.py code
-def normalize_answer(answer):
-    """
-    Remove superflous components to create a normalized form of an answer that
-    can be more easily compared.
-    """
-    from unidecode import unidecode
+# # From class eval.py code
+# def normalize_answer(answer):
+#     """
+#     Remove superflous components to create a normalized form of an answer that
+#     can be more easily compared.
+#     """
+#     from unidecode import unidecode
     
-    if answer is None:
-        return ''
-    reduced = unidecode(answer)
-    reduced = reduced.replace("_", " ")
-    if "(" in reduced:
-        reduced = reduced.split("(")[0]
-    reduced = "".join(x for x in reduced.lower() if x not in string.punctuation)
-    reduced = reduced.strip()
+#     if answer is None:
+#         return ''
+#     reduced = unidecode(answer)
+#     reduced = reduced.replace("_", " ")
+#     if "(" in reduced:
+#         reduced = reduced.split("(")[0]
+#     reduced = "".join(x for x in reduced.lower() if x not in string.punctuation)
+#     reduced = reduced.strip()
 
-    for bad_start in ["the ", "a ", "an "]:
-        if reduced.startswith(bad_start):
-            reduced = reduced[len(bad_start):]
-    return reduced.strip()
+#     for bad_start in ["the ", "a ", "an "]:
+#         if reduced.startswith(bad_start):
+#             reduced = reduced[len(bad_start):]
+#     return reduced.strip()
+
 class QuizBowlModel:
     def __init__(self):
-        self.load_models()
+        model_configs = {
+            'flan-t5-large': {'model': 'google/flan-t5-large', 'tokenizer': 'google/flan-t5-large'},
+            'flan-t5-small': {'model': 'google/flan-t5-small', 'tokenizer': 'google/flan-t5-small'},
+            'flan-t5-base': {'model': 'google/flan-t5-base', 'tokenizer': 'google/flan-t5-base'},
+            'flan-t5-finetuned': {'model': 'dbalasub/finetuned-t5-qanta', 'tokenizer': 'dbalasub/finetuned-t5-qanta'}
 
-    def load_models(self):
-        """Load all models"""
-        # model_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'models', 't5-model-params')
-        # self.load_seq2seq_model(model_dir)
-        self.load_flan_models('google/flan-t5-large', 'google/flan-t5-small')
+        }
+        self.models = {}
+        self.tokenizers = {}
+        self.load_models(model_configs)
 
-    def load_seq2seq_model(self, model_dir):
-        """Load saved models"""
-        self.test_tokenizer = AutoTokenizer.from_pretrained(model_dir)
-        self.test_model = AutoModelForSeq2SeqLM.from_pretrained(model_dir)
-        self.test_model.eval()
-
-    def load_flan_models(self, large_model_id, small_model_id):
-        """Load hugging face models."""        
-        self.tokenizer_flan_t5 = AutoTokenizer.from_pretrained(large_model_id)
-        self.model_flan_t5 = AutoModelForSeq2SeqLM.from_pretrained(large_model_id)
-        self.tokenizer_t5 = AutoTokenizer.from_pretrained(small_model_id)
-        self.model_t5 = AutoModelForSeq2SeqLM.from_pretrained(small_model_id)
+    def load_models(self, model_configs):
+        """Load multiple models based on configuration."""
+        for model_name, config in model_configs.items():
+            tokenizer = AutoTokenizer.from_pretrained(config['tokenizer'])
+            model = AutoModelForSeq2SeqLM.from_pretrained(config['model'])
+            model.eval()
+            self.models[model_name] = model
+            self.tokenizers[model_name] = tokenizer
 
     def guess_and_buzz(self, question_texts):
-        """Generate answers from all models for given questions"""
-        total_answers = self.generate_answers(question_texts)
-        # Display the model's guesses before voting
-        # print("Answers Before Voting Mechanism:")
+        total_answers = [self.generate_answers(question) for question in question_texts]
+        for question, model_answers in zip(question_texts, total_answers):
+            print(f"{question}\nModel Guesses: {model_answers}\n")
+        return [self.ensemble_tfidf_voting(answers) for answers in total_answers]
 
-        # for question, model_answers in zip(question_texts, total_answers):
-        #     print(f"{question}\nModel Guesses: {model_answers}\n\n")
-        return self.ensemble_tfidf_voting(total_answers)
+    def generate_answers(self, question):
+        raw_answers = []
+        for model_name, model in self.models.items():
+            tokenizer = self.tokenizers[model_name]
+            input_ids = tokenizer(question, return_tensors="pt", padding=True, truncation=True).input_ids
+            with torch.no_grad():
+                outputs = model.generate(input_ids, max_new_tokens=5, output_scores=True, return_dict_in_generate=True)
+            decoded_text = tokenizer.decode(outputs.sequences[0], skip_special_tokens=True)
+            confidence_score = self.calculate_confidence(outputs.scores)
+            raw_answers.append((decoded_text, confidence_score))
 
-    def generate_answers(self, question_texts):
-        """Generate answers from each model."""
-        # Tokenize and generate answers using each model
-        return [(self.decode_answer(self.model_flan_t5, self.tokenizer_flan_t5, question),
-                 self.decode_answer(self.model_t5, self.tokenizer_t5, question))
-                for question in question_texts]
-
-    def decode_answer(self, model, tokenizer, input_text):
-        input_ids = tokenizer(input_text, return_tensors="pt", padding=True, truncation=True)
-        with torch.no_grad():
-            outputs = model.generate(**input_ids, max_new_tokens=5, output_scores=True, return_dict_in_generate=True)
+        total_scores = sum([score for _, score in raw_answers])
+        answers = [(text, score / total_scores if total_scores > 0 else 0) for text, score in raw_answers]
         
-        decoded_text = tokenizer.decode(outputs.sequences[0], skip_special_tokens=True)
-        
-        if outputs.scores:
-            log_probs = [torch.nn.functional.log_softmax(score, dim=-1) for score in outputs.scores]
-            scores = []
-            for i in range(len(outputs.sequences[0]) - 1):
-                selected_log_prob = log_probs[i][0, outputs.sequences[0][i + 1]].item()
-                scores.append(selected_log_prob)
-            confidence_score = np.exp(np.mean(scores))
+        return answers
+
+    def calculate_confidence(self, scores):
+        if scores:
+            log_probs = [torch.nn.functional.log_softmax(score, dim=-1) for score in scores]
+            selected_log_probs = [log_probs[i][0, scores[i].argmax()].item() for i in range(len(log_probs))]
+            confidence_score = np.exp(np.mean(selected_log_probs))
         else:
             confidence_score = None
+        return confidence_score
 
-        return decoded_text, confidence_score
+    def ensemble_tfidf_voting(self, answers):
+        return max(answers, key=lambda x: x[1]) if answers else (None, 0)
 
-    def ensemble_tfidf_voting(self, all_answers):
-        """Find answer with highest confidence"""
-        for answers in all_answers:
-            highest_confidence_answer = max(answers, key=lambda x: x[1])
-            yield {'guess': highest_confidence_answer[0], 'confidence': highest_confidence_answer[1]}
+if __name__ == "__main__":
+    # Initialize the QuizBowlModel
+    model = QuizBowlModel()
+    hardcoded_questions = ["Who wrote 'Pride and Prejudice'?", "What is the capital of France?"]
+    # Load questions from a compressed JSON file
+    file_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data', 'qanta.guessdev.json.gz')
+    with gzip.open(file_path, 'rt', encoding='utf-8') as file:
+        data = json.load(file)
+    true_answers = ["Jane Austen", "Paris"]
 
-        # for answers in all_answers:
-        #     texts = [answer[0] for answer in answers]
-            
-        #     vectorizer = TfidfVectorizer()
-        #     tfidf_matrix = vectorizer.fit_transform(texts)
-        #     cosine_scores = cosine_similarity(tfidf_matrix)
-        #     most_similar_index = np.argmax(np.mean(cosine_scores, axis=0))
-        #     yield {'guess': answers[most_similar_index][0], 'confidence': answers[most_similar_index][1]}
+    loaded_questions = [item['text'] for item in data]  
+    true_answers += [item['answer'] for item in data]
+    questions = hardcoded_questions + loaded_questions[:8]  # Increase if needed
+    total_answers = model.guess_and_buzz(questions)
+    
+    print("Final Answers with Voting Mechanism:")
+    # Display the model's after entire ensemble approach
+    for question, model_answers, true_answer in zip(questions, total_answers, true_answers):
+        print(f"{question}\nModel Guesses: {model_answers}\nCorrect Answer: {true_answer}\n\n")
